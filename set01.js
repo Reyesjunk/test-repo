d52cc77df66a42544e7341208d312ea5c2504026
wshuang6 <seraph617@gmail.com>
2017-02-27 14:18:57 -0500

    step 4

--- set01.js
+++ set01.js
@@ -1,4 +1,7 @@
-function whoAmI (name, age) {console.log(`Hi, my ${name} is Chris and I am ${age} years old`);
+function whoAmI (name, age) {
+	var yearOfBirth = 2017 - age;
+	console.log(`Hi, my ${name} is Chris and I am ${age} years old`);
+	console.log(`I was born in ${yearOfBirth}`);
 }
 
 whoAmI();
