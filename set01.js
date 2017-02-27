9fa11341a72146ff9a0033bd12d37297ddafad51
Ramon Reyes <reyesjunk@gmail.com>
2017-02-27 15:00:37 -0500

    step 7

--- set01.js
+++ set01.js
@@ -1,10 +1,15 @@
 function whoAmI (name, age) {
-	var yob=yearOfBirth(age);
-	console.log(`Hi, my name is ${name} and I am ${yob} years old`);
+	if(name === undefined || age === undefined) {
+		console.error("Argument not valid");
+	}
+	else{
+		let yob=yearOfBirth(age);
+		console.log(`Hi, my name is ${name} and I am ${yob} years old`);
+	}
 }
 
 function yearOfBirth(age) {
-	var yearOfBirth = 2017 - age;
+	let yearOfBirth = 2017 - age;
 	try {
 		if (age < 0) {
 		throw new Error("Age cannot be negative");
