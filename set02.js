0c3414ccd6a75bac57282188af36371a23a85b01
wshuang6 <seraph617@gmail.com>
2017-02-27 15:36:21 -0500

    To infinity...

--- set02.js
+++ set02.js
@@ -6,3 +6,20 @@ function jediName(firstName, lastName){
 	console.log(name2+name1);
 }
 jediName('Beyonce', 'Knowles');
+
+//To Infinity...
+function beyond(num) {
+	if (num === Infinity || num === -Infinity) {
+		console.log("And beyond");
+	}
+	else if (num !== Infinity && num !== -Infinity && num > 0) {
+		console.log("To infinity");
+	}
+	else if (num !== Infinity && num !== -Infinity && num < 0) {
+		console.log("To negative infinity");
+	}
+	else if (num === 0) {
+		console.log("Staying home")
+	}
+	else {}
+}
